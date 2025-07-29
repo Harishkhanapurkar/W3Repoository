e7253ccca30fa4c4032c62b4c4ffa88d99de7442
komal <komal@Sachin_Balkar>
2025-07-29 16:23:41 +0530

    this is Javapage commit by Sachin

--- W3School/src/test/java/Test/LoginTest.java
+++ W3School/src/test/java/Test/LoginTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import Base.BaseClass;
+import PageObject.JavaPage;
 //import PageObject.Homepage_Search;
 import PageObject.LoginPage;
 //import PageObject.Tutorial;
@@ -13,10 +14,14 @@ import Utilities.ReadConfig;
 
 public class LoginTest extends BaseClass {
 	LoginPage lp;
+	JavaPage jp;
+
 
 	@Test(priority = 0, dataProvider = "loginData")
 	public void SignInDetail(String emailid, String password) throws InterruptedException {
 		lp = new LoginPage(driver); //this is for login page
+		jp = new JavaPage(driver);
+
 		lp.clickOnSignInbtn();
 		lp.enterEmailId(emailid);
 		lp.enterpass(password);
@@ -26,10 +31,18 @@ public class LoginTest extends BaseClass {
 		Thread.sleep(3000);
 		lp.navigationLogin();
 		
-	
+		// Sachin 
+		jp.clickTopNavJava();
+		Thread.sleep(2000);
+		jp.scrollToTryItAndClick();
+		Thread.sleep(5000);
+		jp.handleTryItWindowAndRun();
+		//jp.clickOnSignInbtn();
+		Thread.sleep(5000);
+		jp.navigationLogin();
+
 	}
 	
-
 	@DataProvider(name = "loginData")
 	public Object[][] loginData() throws IOException {
 		ReadConfig config = new ReadConfig();
