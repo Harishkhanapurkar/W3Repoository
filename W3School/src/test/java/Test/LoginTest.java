f35e831134f7333b1e52499ac246c4631bb8ca4e
vikra <vikra@DESKTOP-ROPQSNP>
2025-07-29 20:03:23 +0530

    I have added new program

--- W3School/src/test/java/Test/LoginTest.java
+++ W3School/src/test/java/Test/LoginTest.java
@@ -1,11 +1,11 @@
 package Test;
 
-
 import java.io.IOException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import Base.BaseClass;
+import PageObject.HomePage_Services;
 //import PageObject.Homepage_Search;
 import PageObject.LoginPage;
 //import PageObject.Tutorial;
@@ -16,7 +16,7 @@ public class LoginTest extends BaseClass {
 
 	@Test(priority = 0, dataProvider = "loginData")
 	public void SignInDetail(String emailid, String password) throws InterruptedException {
-		lp = new LoginPage(driver); //this is for login page
+		lp = new LoginPage(driver); // this is for login page
 		lp.clickOnSignInbtn();
 		lp.enterEmailId(emailid);
 		lp.enterpass(password);
@@ -25,19 +25,36 @@ public class LoginTest extends BaseClass {
 		lp.closePopupIfPresent();
 		Thread.sleep(3000);
 		lp.navigationLogin();
-		
-	
+
+		// VICKY'S CODE STARTED
+
+		HomePage_Services hs = new HomePage_Services(driver);
+
+		hs.clickonservices();
+
+		// To find text is present or not on the web page
+		String text = "All Our Services";
+		if (driver.getPageSource().contains(text))
+			System.out.println("Text is present");
+
+		else
+			System.out.println("Text is not present");
+
+		Thread.sleep(3000);
+
+		hs.ftuturials();
+		hs.clickonsql();
+		hs.nextbtn();
+		hs.previousbtn();
+		hs.navigationLogin();
+
 	}
-	
 
 	@DataProvider(name = "loginData")
 	public Object[][] loginData() throws IOException {
 		ReadConfig config = new ReadConfig();
-		return new Object[][] { { config.getEmailId(), config.getPassword()} };
-		
+		return new Object[][] { { config.getEmailId(), config.getPassword() } };
+
 	}
-	
 
 }
-
-
