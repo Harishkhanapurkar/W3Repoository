c3785b7cdc921a48d2ff07390eecfc4fbbb0cbfb
HP <HP@DESKTOP-3BR6QP1>
2025-07-29 20:23:14 +0530

    Hi Added new Logout PGM

--- W3School/src/test/java/Test/LoginTest.java
+++ W3School/src/test/java/Test/LoginTest.java
@@ -1,6 +1,5 @@
 package Test;
 
-
 import java.io.IOException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -8,6 +7,7 @@ import org.testng.annotations.Test;
 import Base.BaseClass;
 //import PageObject.Homepage_Search;
 import PageObject.LoginPage;
+import PageObject.LogoutPage;
 //import PageObject.Tutorial;
 import Utilities.ReadConfig;
 
@@ -16,7 +16,7 @@ public class LoginTest extends BaseClass {
 
 	@Test(priority = 0, dataProvider = "loginData")
 	public void SignInDetail(String emailid, String password) throws InterruptedException {
-		lp = new LoginPage(driver); //this is for login page
+		lp = new LoginPage(driver); // this is for login page
 		lp.clickOnSignInbtn();
 		lp.enterEmailId(emailid);
 		lp.enterpass(password);
@@ -25,19 +25,24 @@ public class LoginTest extends BaseClass {
 		lp.closePopupIfPresent();
 		Thread.sleep(3000);
 		lp.navigationLogin();
-		
-	
+
+		// Nikhil ----> Now logout
+
+		LogoutPage logout = new LogoutPage(driver);
+		logout.clickOnProfileIcon();
+		Thread.sleep(5000);
+		logout.scrollProfileDropdown();
+		Thread.sleep(2000);
+		logout.clickOnLogout();
+		Thread.sleep(2000);
+
 	}
-	
 
 	@DataProvider(name = "loginData")
 	public Object[][] loginData() throws IOException {
 		ReadConfig config = new ReadConfig();
-		return new Object[][] { { config.getEmailId(), config.getPassword()} };
-		
+		return new Object[][] { { config.getEmailId(), config.getPassword() } };
+
 	}
-	
 
 }
-
-
